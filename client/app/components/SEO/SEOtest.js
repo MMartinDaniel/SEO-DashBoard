c7de9827f10ff11801667a959be1b6b73e0c0658
MMartinDaniel <danmartin.mez@gmail.com>
2019-05-06 16:14:48 +0200

    jjj
    jj

--- client/app/components/SEO/SEOtest.js
+++ client/app/components/SEO/SEOtest.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'whatwg-fetch';
 import {getFromStorage} from "../utils/storage";
 import openSocket from "socket.io-client";
+import SEO from "./SEO";
 
 class Report extends Component {
   constructor(props) {
@@ -9,13 +10,11 @@ class Report extends Component {
     this.state = {};
   }
 
-
-
   render(){
     return(<>
       <div className='test-report'>
         <div className='report-name'>
-          <label><i className="fas fa-check"/></label><strong>  headers</strong>
+          <label><i className="fas fa-check"/></label><strong> {this.props.name} Tag </strong>
           <ul className='report-importance'>
             <li className='full'/><li className='full'/>
             <li className='full'/><li className='full'/>
@@ -23,11 +22,7 @@ class Report extends Component {
           </ul>
         </div>
         <div className='report-info'>
-          <p>Schlitz enamel pin neutra succulents bespoke, dreamcatcher tattooed flannel man bun la
-            croix YOLO readymade chicharrones poke. Selfies ramps brooklyn microdosing church-key
-            sustainable, keytar next level chicharrones PBR&amp;B. Mlkshk schlitz
-            whatever, twee wolf godard banh mi try-hard meh bitters irony. Cronut williamsburg shabby
-            chic, typewriter vape skateboard post-ironic lo-fi YOLO shoreditch iceland.</p>
+          <p>{this.props.details}</p>
         </div>
       </div>
     </>);
@@ -35,24 +30,77 @@ class Report extends Component {
 
 }
 
-class SEOtest extends Component {
-  constructor(props) {
+class HContainer extends Component {
+
+  constructor(props){
     super(props);
-    this.state = {};
+    this.state = {data:null,
+      details:{
+      h: Tags[0].h.info,
+    }
+    };
+  }
+
+  componentWillMount() {
+    const obj = getFromStorage('static');
+    this.setState({
+      uid: obj.uid,
+      website: obj.website,
+    });
+    console.log(obj.website + " and " + obj.uid);
+
+    fetch('/task/h',{
+      method:'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(data => data.json()).then( data =>   this.setState({data: data.data}));
+  }
+
+  render(){
+    const {data,details} = this.state;
+    if(data){
+      return(<>
+        <h6 className={'chart-name spaced'}>{this.props.name}</h6>
+        <div className='seotest-container'>
+          <Report name={"H1"} value={data.h1} details={details.h} />
+          <Report name={"H2"} value={data.h2} details={details.h} />
+          <Report name={"H3"} value={data.h3} details={details.h} />
+        </div>
+      </>);
+    }else{
+      return (<>
+        <div className='seotest-container'/>
+      </>);
+    }
   }
 
 
+}
+
+
+class SEOtest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: null};
+  }
 
   render(){
-    return(<>
 
-      <div className='seotest-container'>
-        <Report/>
-        <Report/>
-        <Report/>
-      </div>
-    </>);
+      return(<>
+        <div className='seotest-container'>
+          <HContainer name='H Tags'/>
+        </div>
+      </>
+      );
   }
 
 }
 export default SEOtest;
+
+const Tags =[
+  {
+    h: {h1: '', h2: 'fas fa-home', h3: '',h4:'',info:'Heading Tags are used to define headings in a  page. h1 to h6 determines the importance that a heading has in the hierarchy.'},
+  },
+];
